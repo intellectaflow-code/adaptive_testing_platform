bb136834c9bcfc175c1b3c8a131176db576cb126
Omkargithub64 <omkkarbhute26@gmail.com>
2026-03-30 17:19:06 +0530

    urls

--- app/main.py
+++ app/main.py
@@ -61,8 +61,8 @@ app = FastAPI(
 # ── CORS – open in dev ────────────────────────────────────────────────────────
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["*","http://localhost:3000", "https://adaptivetestingplatfromteachers-759082157852.asia-south1.run.app","https://adaptivetestingplatfromstudents-759082157852.asia-south1.run.app"],
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000", "https://adaptivetestingplatfromteachers-759082157852.asia-south1.run.app","https://adaptivetestingplatfromstudents-759082157852.asia-south1.run.app"],
+    # allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
